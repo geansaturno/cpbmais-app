8391e569a42b6cc6f07995456adb396abc4d3583
Gean Saturno Gonçalves <cronosnaeg@gmail.com>
2021-06-12 20:23:33 -0300

    Request dos dados das meditações

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,17 +2,50 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import menu from '@/data/menu'
 
+import {
+  ApolloClient,
+  InMemoryCache,
+  gql
+} from '@apollo/client'
+
+const apolloClient = new ApolloClient({
+  uri: 'http://localhost:3050/',
+  cache: new InMemoryCache()
+})
+
 Vue.use(Vuex)
 
 export const state = {
-  menu
+  menu,
+  meditations: []
 }
 
 export default new Vuex.Store({
   state,
   mutations: {
+    setMeditations (state, meditations) {
+      state.meditations = meditations
+    }
   },
   actions: {
+    async getMeditations (context) {
+      const request = await apolloClient.query({
+        query: gql`
+          {
+            meditations {
+              title
+              id
+              description
+              img
+            }
+          }
+        `
+      })
+
+      context.commit('setMeditations', request.data.meditations)
+
+      return context.state.meditations
+    }
   },
   modules: {
   }
--- src/views/Library.vue
+++ src/views/Library.vue
@@ -3,3 +3,19 @@
     <h1>libriray</h1>
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({
+  name: 'Library'
+})
+export default class Library extends Vue {
+  created (): void {
+    console.log('created')
+    this.$store.dispatch('getMeditations').then((meditations) => {
+      console.log('meditations', meditations)
+    })
+  }
+}
+</script>
