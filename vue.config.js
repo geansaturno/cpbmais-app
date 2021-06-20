d5c21e1d9203c46b897e27ffecae0b8c8d0452d9
Gean Saturno Gonçalves <cronosnaeg@gmail.com>
2021-06-20 13:49:44 -0300

    Cache in service worker

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,32 @@
 module.exports = {
+  pwa: {
+    workboxOptions: {
+      runtimeCaching: [
+        {
+          urlPattern: ({ url }) => url.host === 'localhost:3000',
+          method: 'GET',
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'meditations',
+            expiration: {
+              maxAgeSeconds: 6 * 30 * 24 * 60 * 60 // 6 meses
+            }
+          }
+        },
+        {
+          urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'images',
+            expiration: {
+              maxEntries: 10,
+              maxAgeSeconds: 30 * 24 * 60 * 60 // 1 meses
+            }
+          }
+        }
+      ]
+    }
+  },
   chainWebpack: (config) => {
     const svgRule = config.module.rule('svg')
 
